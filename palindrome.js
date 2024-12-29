bfeca320fe699fbf741378f86774d02148704139
Your Name <marielmetalios@gmail.com>
2024-12-29 16:45:57 -0500

    updating Palindrome.js logic

--- palindrome.js
+++ palindrome.js
@@ -12,14 +12,28 @@
 // Input: x = 10
 // Output: false
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-// Given an integer x, return true if x is a palindrome and false otherwise.
+// // Given an integer x, return true if x is a palindrome and false otherwise.
+
+// Take the number 12.
+// Reverse it to get 21.
+// Add the original number (12) to the reversed number (21) to get 33, which is a palindrome
 
 /**
- * @param {number} x
- * @return {boolean}
- */
-const isPalindrome = function(x) {
-    if 
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+// const isPalindrome = function(x) {
+//     for ()
     
-    
-};
+// };
+const num = 12;
+function reverseNumber(num) {
+    // Convert number to string, reverse it, convert back to number
+    return parseInt(num.toString().split('').reverse().join(''));
+  }
+
+const reversed = reverseNumber(num);
+console.log(reversed);
+
+let addNumbers = (num + reversed)
+console.log(addNumbers);
