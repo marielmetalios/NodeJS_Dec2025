44c4c14327c31118679858d52882ff559c76a330
Your Name <marielmetalios@gmail.com>
2024-12-30 21:58:51 -0500

    solved element.js

--- element.js
+++ element.js
@@ -5,16 +5,39 @@
 // Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 // Return k.
 
-/**
- * @param {number[]} nums
- * @param {number} val
- * @return {number}
- */
+// /**
+//  * @param {number[]} nums
+//  * @param {number} val
+//  * @return {number}
+//  */
 
-const val = 3
-const nums = [1,2,3,5,6,7]
+// const val = 3
+// const nums = [1,2,3,5,6,7]
+// var removeElement = function(nums, val) {
+//     return nums.filter(item => item !== val);
+// };
+
+// console.log(nums);
+
+
+// const nums = [3, 2, 2, 3];
+// console.log(nums);
+// const val = 3;
+// const k = removeElement(nums, val);
+
+
+const nums = [3, 2, 2, 3];
+const val = 3;
 var removeElement = function(nums, val) {
-    return nums.filter(item => item !== val);
-};
+    let k=0;
+    for (let i=0; i < nums.length; i++)
+    if (nums[i] !==val) {
+        //move it to the front of array?
+    nums[k] = nums[i];
+    k++;
+    }
+return k;
+}
+const k = removeElement(nums, val);
+console.log(k);
 
-console.log(nums);
